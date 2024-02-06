c105e472cf7bb483490a672a079aa4a405cddf4a
Tchollo88 <Tcholloman@gmail.com>
2024-02-06 13:07:09 -0800

    Finalizing the base requirements project, added comments

--- Holloman_Morgage_Project/Form1.cs
+++ Holloman_Morgage_Project/Form1.cs
@@ -5,14 +5,21 @@ using System.Data;
 using System.Drawing;
 using System.Linq;
 using System.Text;
+using System.Threading;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using static System.Windows.Forms.VisualStyles.VisualStyleElement;
 
 namespace Holloman_Morgage_Project
 {
     public partial class frmMain : Form
     {
+        /** Class - Programming I - IT101  
+          Name - Todd Holloman                       Holloman_Morgage_Solution
 
+            -comment to teacher-
+        I hope the comments help you identify what each part of my code does and helps you navigate it more easily.
+        **/
         
         public frmMain()
         {
@@ -21,11 +28,13 @@ namespace Holloman_Morgage_Project
 
         /**private access codes**/
 
+        //Exit program
         private void btnExit_Click(object sender, EventArgs e)
         {
             this.Close();
         }
 
+        //Clears all fields to initial setup (start-up setup)
         private void btnClear_Click(object sender, EventArgs e)
         {
             this.txtPrinciple.Text = string.Empty;
@@ -36,6 +45,7 @@ namespace Holloman_Morgage_Project
             this.cboInterest.SelectedIndex = -1;
         }
 
+        //Initializes calculation, prints text to labels
         private void btnCalc_Click(object sender, EventArgs e)
         {
             decimal total = 0;
@@ -45,8 +55,10 @@ namespace Holloman_Morgage_Project
             lblTotalNum.Text = total.ToString("C2");
         }
 
+
         /**Public access codes**/
 
+        //Builds the principle variable, or (P)
         public decimal princ()
         {
             /*princ = p*/
@@ -55,7 +67,7 @@ namespace Holloman_Morgage_Project
             return amount;
         }
 
-
+        //Builds the years of agreement variable, or (N)
         public decimal year()
         {
             /*year = n*/
@@ -76,7 +88,7 @@ namespace Holloman_Morgage_Project
             return year;
         }
 
-
+        //Builds the interest rate variable, and converts is to percentage, (R)
         public decimal rate()
         {
             /*rate = r*/
@@ -87,7 +99,7 @@ namespace Holloman_Morgage_Project
             return rate;
         }
 
-
+        //Takes (R) and (N) and converts it into a power variable, or (U)
         public decimal pow_mor(decimal r, decimal n)
         {
             /*pow = u */
@@ -96,26 +108,23 @@ namespace Holloman_Morgage_Project
             return pow;
         }
 
-
+        //Computes the math to the mortgage formula, broke it down to simple terms
         public decimal mtotal(decimal p, decimal r, decimal n, decimal u)
         {
             /*| calc = m | formula m = P[r(1+r)^n/((1+r)^n)-1)] |*/
             //calc = p * (r * (1 + r) * n) / ((1 + r) * n) - 1);
-            decimal top = r * u;
-            
-
-            decimal bott = u - 1;
-            
-
-            decimal rem = top / bott;
-            
-
+            decimal top = r * u;            
+            decimal bott = u - 1;            
+            decimal rem = top / bott;            
             decimal calc = p * rem;
+
+            //Truncates calculation to prep it for currency string
             calc = truncT(calc);
             
             return calc;
         }
 
+        //Truncates to the 10,000th value
         public decimal truncP(decimal r)
         {
             r = r * 10000;
@@ -125,6 +134,7 @@ namespace Holloman_Morgage_Project
             return r;
         }
 
+        //Truncates to the 100th value
         public decimal truncT(decimal r)
         {
             r = r * 100;
